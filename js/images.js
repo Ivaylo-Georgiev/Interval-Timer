c58daa570b9338e8050865e90f03c851a7a15814
Ivaylo <ivaylo.georgiev18@gmail.com>
2019-10-01 08:46:12 +0300

    Create images.js

---
+++ js/images.js
@@ -0,0 +1,6 @@
+const playImgChalk = "../images/play_chalk.svg";
+const pauseImgChalk = "../images/pause_chalk.svg";
+const resetImgChalk = "../images/reset_chalk.svg";
+const playImgGraphite = "../images/play_graphite.svg";
+const pauseImgGraphite = "../images/pause_graphite.svg";
+const resetImgGraphite = "../images/reset_graphite.svg"
