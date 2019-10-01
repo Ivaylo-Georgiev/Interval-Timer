64e897129938938b7dd8c46132c526c76be1fe73
Ivaylo <ivaylo.georgiev18@gmail.com>
2019-10-01 08:45:50 +0300

    Create colors.js

---
+++ js/colors.js
@@ -0,0 +1,17 @@
+const graphiteRGB="rgb(59, 59, 72)";
+const chalkBlueRGB="rgb(204, 218, 215)";
+const mimosaRGB="rgb(240, 192, 90)";
+
+const graphite="#3B3B48";
+const chalkBlue="#CCDAD7";
+
+const chiliPepper="#9B1B30";
+const marsala="#964F4C";
+const livingCoral="#FF6F61";
+const ultraViolet="#5F4B8B";
+const mimosa="#F0C05A";
+const greenery="#88B04B";
+const emerald="#009473";
+const turquoise="#45B5AA";
+const serenity="#91A8D0";
+const blueIris="#5A5B9F";
